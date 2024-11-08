f8ef9ac23bede7f95269bce7bcd70206e7d58b28
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-11-08 21:14:06 +0800

    Project Restructure

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-g++ -std=c++17 -Iinclude main.cpp lib/*.cpp lib/helper/*.cpp lib/menus/*.cpp -o main && .\main
+g++ -std=c++17 -Iinclude/core -Iinclude/data_structures -Iinclude/email -Iinclude/services -Iinclude/ui -Iinclude/utils main.cpp lib/core/*.cpp lib/data_structures/*.cpp lib/email/*.cpp lib/services/*.cpp lib/ui/*.cpp lib/utils/*.cpp -o main && .\main
